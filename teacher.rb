e7d0f57e2d8691eec9022072df2fe5fd5a79cb18
Ikenna Richard <oguejioforrichard@gmail.com>
2023-06-19 19:09:49 -1100

    Add teacher blueprint

---
+++ teacher.rb
@@ -0,0 +1,15 @@
+require './person.rb'
+
+class Teacher < Person
+
+  def initialize(specialization, name='Unknown', age)
+    super(name, age)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
+
+nneka = Teacher.new('Arts', 'Nneka', 29)
