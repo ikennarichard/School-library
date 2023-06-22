5592827ae686a64726d2b663b78b117b42cc3694
Ikenna Richard <oguejioforrichard@gmail.com>
2023-06-21 19:05:35 -1100

    Add classroom blueprint

---
+++ classroom.rb
@@ -0,0 +1,15 @@
+class Classroom
+
+  attr_accessor :label
+    
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    student.classroom = self
+    @students << student
+  end
+
+end
