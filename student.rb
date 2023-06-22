be8ad40a7994b8c7ae6e740c744ebf23fdb9843b
Ikenna Richard <oguejioforrichard@gmail.com>
2023-06-21 19:01:19 -1100

    Add relationship with classroom

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require './person'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission = true)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -9,4 +11,5 @@ class Student < Person
   def play_hooky
     '¯(ツ)/¯'
   end
+
 end
