54c3e2e59d721f8c37be5973fb0392f0a0f138a2
Ikenna Richard <oguejioforrichard@gmail.com>
2023-06-21 19:05:57 -1100

    Add rental blueprint

---
+++ rental.rb
@@ -0,0 +1,14 @@
+class Rental
+
+  attr_accessor :date, :book, :person
+
+  def initialiize(date)
+    @date = date
+
+    @person = person
+    person.rentals << self
+    
+    @book = book
+    book.rentals << self
+  end
+end
