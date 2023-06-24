07c5d6f8951f9d8d5b93108d189905b5a0cd5f57
Ikenna Richard <oguejioforrichard@gmail.com>
2023-06-23 16:41:18 -1100

    Refactor code

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
-require_relative './book'
-require_relative './classroom'
-require_relative './person'
-require_relative './rental'
-require_relative './student'
-require_relative './teacher'
+require_relative 'book'
+require_relative 'classroom'
+require_relative 'person'
+require_relative 'rental'
+require_relative 'student'
+require_relative 'teacher'
 
 class App
   attr_accessor :people, :books, :rentals
