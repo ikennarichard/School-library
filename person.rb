21423a537a8fc2ea9e8b0be81aa7238c9ab0d57a
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-22 17:37:54 -1100

    Refactor code

--- person.rb
+++ person.rb
@@ -1,13 +1,7 @@
-class Nameable
-  def correct_name
-    raise NotImplementedError,
-          "#{self.class} has not implemented method '#{__method__}'"
-  end
-end
+require_relative 'nameable'
 
 class Person < Nameable
-  attr_accessor :name, :age, :rentals
-  attr_reader :id
+  attr_accessor :name, :age, :rentals, :parent_permission, :id
 
   def initialize(age, name = 'Unknown', parent_permission = true)
     @id = Random.rand(1...100)
@@ -25,38 +19,13 @@ class Person < Nameable
     @name
   end
 
-  def add_rental(book, date)
-    Rental.new(date, book, self)
+  def add_rental(rental)
+    @rentals << rental
   end
 
   private
 
   def of_age?
     @age >= 18
-  end
-end
-
-class Decorator < Nameable
-  attr_accessor :nameable
-
-  def initialize(nameable)
-    @nameable = nameable
-  end
-
-  def correct_name
-    @nameable.correct_name
-  end
-end
-
-class CapitalizeDecorator < Decorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < Decorator
-  def correct_name
-    text = @nameable.correct_name
-    text.length <= 10 ? text : text[0..9]
   end
 end
