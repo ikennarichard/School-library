661a9bafc62d8c9ff75483a0f06042859ec77acb
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-22 17:32:24 -1100

    Move decoratos to a seperate file

---
+++ decorator.rb
@@ -0,0 +1,26 @@
+require_relative 'nameable'
+
+class Decorator < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    text = @nameable.correct_name
+    text.length <= 10 ? text : text[0..9]
+  end
+end
